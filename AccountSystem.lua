51331f0f33e836ca1a32171955b4d7ccfd78c4ca
Synioxzz <alejandrogarciasanta1@gmail.com>
2025-12-05 14:27:44 +0100

    Update AccountSystem.lua

--- AccountSystem.lua
+++ AccountSystem.lua
@@ -3,82 +3,104 @@ return {
         {
             Username = "SynV4",
             Password = "NIGGERS",
-            IsActive = true
+            IsActive = true,
+            HWID = "9d42450e1769f7f73eb03c4055cdbe84901222d55f4d53"
         },
         {
             Username = "Dexter",
             Password = "Tamarind12@",
-            IsActive = true
+            IsActive = true,
+            HWID = nil
         },
         {
             Username = "Sxvy",
             Password = "Sxvy1234",
-            IsActive = true
+            IsActive = true,
+            HWID = nil
         },
         {
             Username = "Kiwi",
             Password = "Rankgame55",
-            IsActive = true
+            IsActive = false,
+            HWID = nil
         },
         {
             Username = "Mafia",
             Password = "mafiaclantop1clan",
-            IsActive = true
+            IsActive = true,
+            HWID = nil
         },
         {
             Username = "Desire",
             Password = "FuckKiwi",
-            IsActive = true
+            IsActive = true,
+            HWID = "41573bb15ad12f343eefa37fdd1703fa8c7eb4765f4d53"
         },
         {
             Username = "Abstract",
             Password = "abstractspunyoblock",
-            IsActive = true
+            IsActive = true,
+            HWID = nil
         },
         {
             Username = "Croc",
             Password = "12345678",
-            IsActive = true
+            IsActive = true,
+            HWID = nil
         },
         {
             Username = "Breezy",
             Password = "thegoatbreezy1234@",
-            IsActive = false
+            IsActive = true,
+            HWID = nil
         },
         {
             Username = "theerm",
             Password = "theerm1234",
-            IsActive = false
+            IsActive = false,
+            HWID = nil
         },
         {
             Username = "GWY",
             Password = "uzzalovesmydick",
-            IsActive = true
+            IsActive = true,
+            HWID = "1d0055db51492e3bb8feaaf86e2ef9440f7739cd87c630af6fe04e5c294fe854"
         },
         {
             Username = "Minhal",
             Password = "coolsigma123",
-            IsActive = true
+            IsActive = true,
+            HWID = nil
         },
         {
             Username = "Chase",
             Password = "SigmaFish",
-            IsActive = true
+            IsActive = true,
+            HWID = nil
         },
         {
             Username = "Crobo",
             Password = "TripleCrobo",
-            IsActive = true
+            IsActive = true,
+            HWID = "7816ef8823cd02587677311535ff90b103a32c285f4d53"
+        },
+        {
+            Username = "ShogunThePrime",
+            Password = "ShogunThePrime1234",
+            IsActive = true,
+            HWID = "7d17ff85ac9cf0d6a6eec2a238b0ef6b9fc08b8cae115679acf6b267fe9f5add"
         },
         {
             Username = "Doctor",
             Password = "DoctorScatPorn",
-            IsActive = true
+            IsActive = true,
+            HWID = nil
         },
         {
             Username = "stxnned",
             Password = "stxnned12345",
-            IsActive = true
+            IsActive = true,
+            HWID = nil
         }
     }
 }
